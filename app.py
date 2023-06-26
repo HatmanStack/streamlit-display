09212ff5dcfe229e0e708840252ad64e2f1fc4ea
HatmanStack <82614182+HatmanStack@users.noreply.github.com>
2023-06-26 09:26:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,12 @@ st.markdown("<h1 style='text-align: center; color: steelblue;'>Invite Results</h
 
 def get_cur():
   conn = st.experimental_connection('snowflake', type='sql')
-  data = get_party_invites(conn)
-  my_cnx.close()
+  data = conn.query("select * from SHEETS", ttl=600)
   df = pandas.DataFrame(data)
   time_stamp = df.iloc[:,0]
   altered_time = [i.split(' ')[0] for i in time_stamp]
   df.iloc[:,0] = altered_time
   st.table(df)
 
-
-def get_party_invites(my_cnx):
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from SHEETS")
-    return my_cur.fetchall()
 
 get_cur()
