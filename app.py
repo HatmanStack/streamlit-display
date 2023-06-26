afad2ed018c69ffc900bc36d4f05546d3c5ffaa6
HatmanStack <82614182+HatmanStack@users.noreply.github.com>
2023-06-26 09:30:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-import streamlit as st
+import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 
-
-st.markdown("<h1 style='text-align: center; color: steelblue;'>Invite Results</h1>", unsafe_allow_html=True)
+streamlit.markdown("<h1 style='text-align: center; color: steelblue;'>Invite Results</h1>", unsafe_allow_html=True)
 
 def get_cur():
-  conn = st.experimental_connection('snowflake', type='sql')
-  data = conn.query("select * from SHEETS", ttl=600)
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  data = get_party_invites(my_cnx)
+  my_cnx.close()
   df = pandas.DataFrame(data)
   time_stamp = df.iloc[:,0]
   altered_time = [i.split(' ')[0] for i in time_stamp]
   df.iloc[:,0] = altered_time
-  st.table(df)
+  streamlit.table(df)
 
+
+def get_party_invites(my_cnx):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from SHEETS")
+    return my_cur.fetchall()
 
 get_cur()
